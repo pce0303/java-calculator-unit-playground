46733624546110db7f79d92a83857f3145a781e0
pce0303 <ivychoen@gmail.com>
2026-03-22 20:55:21 +0900

    feat: 예외 처리 테스트 메서드 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -9,12 +9,42 @@ class StringCalculatorTest {
 
     @Test
     @DisplayName("구분자를 기준으로 분리한 각 숫자의 합을 얻을 수 있다.")
-    void testString() {
+    void customDelimiter() {
         // given
         StringCalculator stringCalculator = new StringCalculator();
 
         // when
         String[] parsed = stringCalculator.parser("//;\n1;2;3");
+        String[] tokens = stringCalculator.validator(parsed);
+        int actual = stringCalculator.add(tokens);
+
+        // then
+        assertThat(6).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("음수가 입력되었을 때 런타임 에러를 확인할 수 있다.")
+    void negativeNumber() {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        String[] parsed = stringCalculator.parser("//;\n1;-2;3");
+        String[] tokens = stringCalculator.validator(parsed);
+        int actual = stringCalculator.add(tokens);
+
+        // then
+        assertThat(6).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자가 입력되었을 때 런타임 에러를 확인할 수 있다.")
+    void specialChar() {
+        // given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        // when
+        String[] parsed = stringCalculator.parser("//;\n1;?2;3");
         String[] tokens = stringCalculator.validator(parsed);
         int actual = stringCalculator.add(tokens);
 
